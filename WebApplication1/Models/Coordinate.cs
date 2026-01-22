2e98939771555a8a9cbbb184b2db25c5641a1740
Cup0f <peterpolyak2000@gmail.com>
2026-01-22 15:30:46 +0100

    Domain model hozzáadása

---
+++ WebApplication1/Models/Coordinate.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApplication1.Models;
+
+public class Coordinate
+{
+    public int Id { get; set; }
+    public double Latitude { get; set; }
+    public double Longitude { get; set; }
+    public string? Name { get; set; } 
+    public int Order { get; set; }
+}
