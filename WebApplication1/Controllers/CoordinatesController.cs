e590d799f2616e6d9b632c617d21152e0ae52ebb
Cup0f <peterpolyak2000@gmail.com>
2026-01-22 15:33:38 +0100

    GET endpointok (read)

---
+++ WebApplication1/Controllers/CoordinatesController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebApplication1.Data;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+
+public class CoordinatesController : ControllerBase
+{ 
+    private readonly AppDbContext _context;
+    
+    public CoordinatesController(AppDbContext context)
+    {
+        _context = context;
+    }
+   
+   [HttpGet]
+   public async Task<ActionResult<IEnumerable<Coordinate>>> GetAll()
+   {
+       var coords = await _context.Coordinates
+           .OrderBy(c => c.Id)
+           .ToListAsync();
+
+       return Ok(coords);
+   }
+
+   [HttpGet("{id:int}")]
+   public async Task<ActionResult<Coordinate>> GetById(int id)
+   {
+       var coord = await _context.Coordinates.FindAsync(id);
+       if (coord == null) return NotFound();
+       return Ok(coord);
+   }
+}
