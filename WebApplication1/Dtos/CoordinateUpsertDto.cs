1a89e22c4805b13bdaaee6c98a9df465ccdf9dbb
Cup0f <peterpolyak2000@gmail.com>
2026-01-22 15:34:44 +0100

    DTO bevezetése

---
+++ WebApplication1/Dtos/CoordinateUpsertDto.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApplication1.Dtos;
+
+public class CoordinateUpsertDto
+{
+    public double Latitude { get; set; }
+    public double Longitude { get; set; }
+    public string? Name { get; set; }
+    public int Order { get; set; }
+}
