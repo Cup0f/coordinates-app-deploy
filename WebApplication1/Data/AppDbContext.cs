8e842397367bc97f3d6bd0831848beb86c7681e0
Cup0f <peterpolyak2000@gmail.com>
2026-01-22 15:32:10 +0100

    DbContext + PostgreSQL integráció

---
+++ WebApplication1/Data/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApplication1.Models;
+
+namespace WebApplication1.Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+    public DbSet<Coordinate> Coordinates => Set<Coordinate>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Coordinate>().ToTable("coordinates");
+
+        modelBuilder.Entity<Coordinate>()
+            .Property(c => c.Order)
+            .HasColumnName("order");
+    }
+}
