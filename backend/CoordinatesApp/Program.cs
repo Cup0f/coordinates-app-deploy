063cea928ba73c13fc0f65024f77b69e94659097
Cup0f <peterpolyak2000@gmail.com>
2026-01-29 16:38:36 +0100

    allow any origin

--- backend/CoordinatesApp/Program.cs
+++ backend/CoordinatesApp/Program.cs
@@ -16,7 +16,7 @@ builder.Services.AddCors(options =>
     options.AddPolicy("FrontendDev", policy =>
     {
         policy
-            .WithOrigins("http://localhost:5173")
+            .AllowAnyOrigin()
             .AllowAnyHeader()
             .AllowAnyMethod();
     });
